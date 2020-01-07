57167595d2ce1ef337067c3ed448005f2e1315ae
verbanicm <verbanicm@google.com>
2020-01-07 13:04:10 -0500

    More License files, etc

--- cleanup-project.sh
+++ cleanup-project.sh
@@ -16,7 +16,7 @@
 
 source ./vars.sh
 
-gcloud container clusters delete $CLUSTER_NAME --zone $ZONE
+gcloud container clusters delete $CLUSTER_NAME --zone $ZONE --quiet
 
 gcloud container images delete $CONTAINER_PATH --force-delete-tags --quiet
 
@@ -25,5 +25,3 @@ gcloud container binauthz attestors delete $ATTESTOR_ID
 curl -vvv -X DELETE \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     "https://containeranalysis.googleapis.com/v1/projects/${GOOGLE_CLOUD_PROJECT}/notes/${NOTE_ID}"
-
-gcloud container binauthz policy import files/allow_all_policy.yaml
